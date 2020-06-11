63b6405c743f12e34fce494881bff1bd75113919
khantmawhein <kokhant228abc@gmail.com>
2020-06-11 09:52:09 +0630

    Updates

---
+++ precache-manifest.7d1ba0e7bd2ef8eb8bc6afafbced9f8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29320fae8274216727b7cecb24391bb2",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "2709f584b277af9838df",
+    "url": "/my-todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d54b42b77dfdbd4f362",
+    "url": "/my-todo-app/static/js/2.bac48d91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-todo-app/static/js/2.bac48d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2709f584b277af9838df",
+    "url": "/my-todo-app/static/js/main.fe610988.chunk.js"
+  },
+  {
+    "revision": "f7ba73de4bd3bc408df9",
+    "url": "/my-todo-app/static/js/runtime-main.3311b1d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
